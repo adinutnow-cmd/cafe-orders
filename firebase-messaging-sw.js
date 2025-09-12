cf69122376bb7e855d68c1a0a137815c0c1d00e4
adinutnow-cmd <adinutnow@gmail.com>
2025-09-12 14:40:47 +0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+// Service Worker للتنبيهات بالخلفية
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDR9hkeAE5jn-Ojoq7ANMA28EKBlGWRwNQ",
+  authDomain: "cafeorders-3cd97.firebaseapp.com",
+  databaseURL: "https://cafeorders-3cd97-default-rtdb.firebaseio.com",
+  projectId: "cafeorders-3cd97",
+  storageBucket: "cafeorders-3cd97.firebasestorage.app",
+  messagingSenderId: "1066008207283",
+  appId: "1:1066008207283:web:016435ecd8c9ba327d35c6",
+  measurementId: "G-TY9137GQ82"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("📩 Background message: ", payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon-192.png"
+  });
+});
