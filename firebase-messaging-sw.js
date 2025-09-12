1681418b7757784eada457a0b49ca415d371d1d8
adinutnow-cmd <adinutnow@gmail.com>
2025-09-12 15:08:15 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,16 @@
 // Service Worker للتنبيهات بالخلفية
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyDR9hkeAE5jn-Ojoq7ANMA28EKBlGWRwNQ",
   authDomain: "cafeorders-3cd97.firebaseapp.com",
-  databaseURL: "https://cafeorders-3cd97-default-rtdb.firebaseio.com",
   projectId: "cafeorders-3cd97",
-  storageBucket: "cafeorders-3cd97.firebasestorage.app",
+  storageBucket: "cafeorders-3cd97.appspot.com",
   messagingSenderId: "1066008207283",
   appId: "1:1066008207283:web:016435ecd8c9ba327d35c6",
   measurementId: "G-TY9137GQ82"
-};
-
-firebase.initializeApp(firebaseConfig);
+});
 
 const messaging = firebase.messaging();
 
