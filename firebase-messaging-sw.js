3049f5c87a651a42a74e59148ff64b8d50979d60
adinutnow-cmd <adinutnow@gmail.com>
2025-09-12 15:51:59 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,27 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js");
+// Use Firebase v8 inside Service Worker
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
+console.log("🛠 Service Worker loaded");
+
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDR9hkeAE5jn-Ojoq7ANMA28EKBlGWRwNQ",
   authDomain: "cafeorders-3cd97.firebaseapp.com",
+  databaseURL: "https://cafeorders-3cd97-default-rtdb.firebaseio.com",
   projectId: "cafeorders-3cd97",
-  storageBucket: "cafeorders-3cd97.appspot.com",
+  storageBucket: "cafeorders-3cd97.firebasestorage.app",
   messagingSenderId: "1066008207283",
   appId: "1:1066008207283:web:016435ecd8c9ba327d35c6",
   measurementId: "G-TY9137GQ82"
 };
 
 firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
-// Background message handler
 messaging.onBackgroundMessage((payload) => {
-  console.log("Background message received: ", payload);
+  console.log("📦 Background message received:", payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
     icon: "/icon-192.png"
