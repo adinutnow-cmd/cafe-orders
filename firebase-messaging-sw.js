8e00558ade3872becfdf6387a21c5e12f444aad5
adinutnow-cmd <adinutnow@gmail.com>
2025-09-12 15:31:28 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,7 @@
-// Service Worker للتنبيهات بالخلفية
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js");
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyDR9hkeAE5jn-Ojoq7ANMA28EKBlGWRwNQ",
   authDomain: "cafeorders-3cd97.firebaseapp.com",
   projectId: "cafeorders-3cd97",
@@ -10,12 +9,14 @@ firebase.initializeApp({
   messagingSenderId: "1066008207283",
   appId: "1:1066008207283:web:016435ecd8c9ba327d35c6",
   measurementId: "G-TY9137GQ82"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
+// Background message handler
 messaging.onBackgroundMessage((payload) => {
-  console.log("📩 Background message: ", payload);
+  console.log("Background message received: ", payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
     icon: "/icon-192.png"
